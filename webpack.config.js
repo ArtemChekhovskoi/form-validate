2accb410b244f561570d3c8b18404501fc699549
Artem Chekhovskoi <neomonreo@gmail.com>
2023-03-31 21:46:33 +0300

    package.json added to dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = ({ development }) => ({
-    entry: './src/validate.ts',
-    devtool: development ? 'inline-source-map' : false,
-    mode: development ? 'development' : 'production',
-    output: {
-        filename: 'validate.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'form-validate',
-        libraryExport: 'default',
-        libraryTarget: 'umd',
-        umdNamedDefine: true,
-        globalObject: 'typeof self === \'undefined\' ? this : self',
-    },
-    resolve: {
-        extensions: ['.ts'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'ts-loader'],
-            },
-        ],
-    }
-})
+  entry: "./src/validate.ts",
+  devtool: development ? "inline-source-map" : false,
+  mode: development ? "development" : "production",
+  output: {
+    filename: "validate.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "form-validate",
+    libraryExport: "default",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+    globalObject: "typeof self === 'undefined' ? this : self",
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "package.json"),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+});
